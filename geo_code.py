fd55ddc57f8773ab11414ef4e040a6c7b1e0792a
hchung1 <hchung1@live.nmhu.edu>
2018-09-12 14:27:36 -0600

    Posting Assignment

---
+++ geo_code.py
@@ -0,0 +1,37 @@
+from geopy.geocoders import Nominatim
+from geopy.distance import lonlat, distance
+
+def geo_locator(query):
+  geolocator = Nominatim(user_agent="my_application")
+  location = geolocator.geocode(query)
+  pos = (location.longitude, location.latitude)
+  return pos
+
+def result(loc1, loc2, one, two):
+  x = (distance(lonlat(*one), lonlat(*two)).miles)
+  print ("Distance between %s and %s is %s miles." % (loc1, loc2, str(x)))
+
+#def test():
+#  query = '1314 Chavez St, las vegas, nm'
+#  query1 = '11105 walnut st, los angeles, ca'
+#  print ("Testing: %s and %s on the geopy software." % (query, query1))
+#  one = geo_locator(query)
+#  two = geo_locator(query1)
+#  result(query, query1, one,two)
+#test()
+def starting():
+  pos1 = str(raw_input("Enter starting location: "))
+  pos2 = str(raw_input("Enter ending location: "))
+  one = geo_locator(pos1)
+  two = geo_locator(pos2)
+  result(pos2, pos1, one,two)
+  
+command = {1:starting, 2:exit}
+a = True
+while a == True:
+  try:
+    ans = int(input("Enter 1 to start.\nEnter 2 to quit.\n>>"))
+    command[ans]()
+  except:
+    pass
+
